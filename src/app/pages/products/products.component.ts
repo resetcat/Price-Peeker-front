d12856053038e1146b5c602b0fcb791b899c18bf
Mihails Dudarevs <deadcatofdoom@googlemail.com>
2023-11-20 13:47:09 +0200

    basic  cards

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductDto } from 'src/app/models/products.dto';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: ProductDto[] = [];
 
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productService.products$.subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SearchDto } from '../models/search.dto';
+import { BehaviorSubject } from 'rxjs';
+import { ProductDto } from '../models/products.dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  constructor(private httpclient: HttpClient) {}
+  private productsSource = new BehaviorSubject<any[]>([]);
+  products$ = this.productsSource.asObservable();
+
+  constructor(private httpClient: HttpClient) {}
 
   getProducts(query: string) {
-    const body: SearchDto = {
-      query: query,
-    };
-    this.httpclient
-      .post('http://localhost:3080/grocery', body)
-      .subscribe((v) => console.info(v));
+    const body: SearchDto = { query: query };
+    this.httpClient
+      .post<ProductDto[]>('http://localhost:3080/grocery', body)
+      .subscribe(
+        (data) => {
+          this.productsSource.next(data);
+        },
+        (error) => {
+          console.error(error);
+          this.productsSource.next([]);
+        }
+      );
   }
 }
